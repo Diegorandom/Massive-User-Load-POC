02429d71e5280faf4e0511529a38e0010a5c7a13
Diego Ignacio Ortega Martínez <diegortegaimi@gmail.com>
2023-05-27 14:10:51 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const config = {
-	AUTHORITY_HOST_URL: 'https://login.microsoftonline.com',
-    TENANT: 'solidarityUAT.onmicrosoft.com',//tenant_id || tenant
-    CLIENT_ID: 'dce90411-b52a-48df-adbd-a76b4f7f4b3a',
-    CLIENT_SECRET: 'Lnilj31xr.+L7bG_*xC45pLzw6agsz=F',
-    RESOURCE: 'https://graph.windows.net',
-    APPLICATION_CLIENT_ID: "58b4bce74aa94332aade9a6c26960b24",
-    RESOURCE_MICROSOFT_API : "https://graph.microsoft.com/"
+    AUTHORITY_HOST_URL: '',
+    TENANT: '',
+    CLIENT_ID: '',
+    CLIENT_SECRET: '',
+    RESOURCE: '',
+    APPLICATION_CLIENT_ID: '',
+    RESOURCE_MICROSOFT_API : '',
 };
 
 module.exports = config;
